42c98d513dc0b7f89fb20c94148ebac34376228d
Kim <kevin2695@naver.com>
2026-01-09 18:57:40 +0900

    feat : svg index 추가

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as TopIcon } from "./icon-position-top.svg";
+export { default as JungleIcon } from "./icon-position-jungle.svg";
+export { default as MidIcon } from "./icon-position-middle.svg";
+export { default as BotIcon } from "./icon-position-bottom.svg";
+export { default as SupportIcon } from "./icon-position-utility.svg";
